16dd684b38f6e4900e4da3e4a2b6a5ed4261e572
Angel Jara <angel.jara@bongous.com>
2016-03-10 23:58:55 +0100

    update

--- ws/Api/src/Api/Model/Gateway/Carrier/UspsCarrier.php
+++ ws/Api/src/Api/Model/Gateway/Carrier/UspsCarrier.php
@@ -149,7 +149,7 @@ class UspsCarrier extends CarrierAbstract
             
     public function isSearchKeyOwner($searchkey)
     {
-        return true;
+        return false;
         $return = false;
         if (preg_match('/^([0-9]{20})?([0-9]{4}[0-9]{4}[0-9]{4}[0-9]{2})$/', $searchkey)) {
             $return = true;
