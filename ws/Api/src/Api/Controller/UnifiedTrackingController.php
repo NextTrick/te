592f0ffad445eba5918e6c4052d044182aa48a14
unknown <jared.cusi@TP024.crossborderlima.com>
2016-03-11 10:01:50 -0500

    maps

--- ws/Api/src/Api/Controller/UnifiedTrackingController.php
+++ ws/Api/src/Api/Controller/UnifiedTrackingController.php
@@ -7,7 +7,6 @@ use Service\Model\Repository\ServiceRepository;
 
 class UnifiedTrackingController extends BaseRestfulController
 {
-    const ENDPOINT_UNIFIED_TRACKING_ID = ServiceRepository::ENDPOINT_UNIFIED_TRACKING_ID;
     
     public function getList()
     {
@@ -21,42 +20,16 @@ class UnifiedTrackingController extends BaseRestfulController
 
     public function create($params)
     {
-        $params = $this->getRequestParams();
-        $params['serviceId'] = self::ENDPOINT_UNIFIED_TRACKING_ID;
-        $params['apikeyId'] = $this->apikeyId;
-        $params['trackings'] = $params['trackings'];
-        $params['profileId'] = $this->apikeyData['profileId'];
-        
-        return $this->getMultiTrackingService()
-                   ->insertMultiTracking($params);        
+        return array();       
     }
 
     public function update($token, $params)
     {
-        $params = $this->getRequestParams();
-        $params['serviceId'] = self::ENDPOINT_UNIFIED_TRACKING_ID;
-        $params['apikeyId'] = $this->apikeyId;     
-        $params['trackings'] = $params['trackings'];
-        $params['profileId'] = $this->apikeyData['profileId'];
-       
-        return $this->getMultiTrackingService()
-                   ->updateMultiTracking($token, $params); 
+       return array();
     }
 
     public function delete($token)
     {
-        $params = $this->getRequestParams();
-        $params['serviceId'] = self::ENDPOINT_UNIFIED_TRACKING_ID;
-        $params['apikeyId'] = $this->apikeyId;     
-        $params['profileId'] = $this->apikeyData['profileId'];
-        return $this->getMultiTrackingService()->deleteMultiTracking($token, $params);
-    }
-   
-    /**
-     * @return \Service\Model\Service\MultiTrackingService
-     */
-    public function getMultiTrackingService()
-    {
-        return $this->getServiceLocator()->get('Model\MultiTrackingService');
+        return array();
     }
 }
