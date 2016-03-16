60e751f859b304e313e9aba714a6fb16641d2fbd
unknown <jared.cusi@TP024.crossborderlima.com>
2016-03-16 17:41:51 -0500

    update ca ada post

--- scripts/sql/001_masterdata_trackingengine.sql
+++ scripts/sql/001_masterdata_trackingengine.sql
@@ -2,7 +2,7 @@
 INSERT INTO fcb_carrier_carrier (carrierId, name, status, creationDate, alias) VALUES (1, 'Fedex', 1, now(), 'Fedex');
 INSERT INTO fcb_carrier_carrier (carrierId, name, status, creationDate, alias) VALUES (2, 'DHL', 1, now(), 'Dhl');
 INSERT INTO fcb_carrier_carrier (carrierId, name, status, creationDate, alias) VALUES (3, 'Ups', 1, now(), 'Ups');
-INSERT INTO fcb_carrier_carrier (carrierId, name, status, creationDate, alias) VALUES (4, 'CanadaPost', 0, now(), 'CanadaPost');
+INSERT INTO fcb_carrier_carrier (carrierId, name, status, creationDate, alias) VALUES (4, 'CanadaPost', 1, now(), 'CanadaPost');
 INSERT INTO fcb_carrier_carrier (carrierId, name, status, creationDate, alias) VALUES (5, 'Usps', 1, now(), 'Usps');
 INSERT INTO fcb_carrier_carrier (carrierId, name, status, creationDate, alias) VALUES (6, 'FedexCrossBorder', 1, now(), 'FedexCrossBorder');
 
