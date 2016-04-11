a0aacbc026ffcdc7e83a1bb9c07392f7dd40650d
unknown <jared.cusi@TP024.crossborderlima.com>
2016-04-11 02:19:07 -0500

    update ubigeo tracking

--- module/Track/src/Track/Model/Service/EventService.php
+++ module/Track/src/Track/Model/Service/EventService.php
@@ -2,8 +2,53 @@
 namespace Track\Model\Service;
 
 use Util\Model\Service\Base\AbstractService;
-
+use Track\Model\Repository\EventRepository;
 class EventService extends AbstractService
 {
+    public function saveEvent($params)
+    {
+        $dataTrack = $this->getTrackService()->getRepository()
+                        ->getBy(array('trackId=?' => $params['trackId']));
+        if(empty($dataTrack)) {
+            return array(
+                'success' => FALSE,
+                'message' => 'TrackId no valido'
+            );
+        }
+        $response = array('success' => TRUE);
+        $eventId = $this->getRepository()->save(array(
+            'eventId' => empty($params['eventid'])? NULL : $params['eventid'],
+            'trackId' => $params['trackId'], 
+            'eventStatusId'  => $params['eventStatusId'], 
+            'creationDate' => date('Y-m-d H:i:s'), 
+            'postalCode' => $params['trackId'],  
+            'stateProvinceCode' => $params['trackId'],  
+            'countryName' => $params['countryName'],  
+            'countryCode' => $params['countryCode'],  
+            'latitud' => $params['latitud'],  
+            'longitud' => $params['longitud'],  
+            'ubigeoId' => $params['ubigeoId'], 
+        ));
+        $response['data']['eventId'] = $eventId;
+        return $response;
+    }
+    
+    public function deleteEvent($params)
+    {
+        $response = array('success' => TRUE);
+        $this->getRepository()->update(
+                array('status' => EventRepository::STATUS_DISABLED),
+                array('eventId=?' => $params['eventId'])
+            );
+        return $response;
+    }
+    
+    /**
+     * @return \Track\Model\Service\TrackService
+     */
+    public function getTrackService()
+    {
+        return $this->getServiceLocator()->get('Track\Model\Service\TrackService');
+    }
     
 }
